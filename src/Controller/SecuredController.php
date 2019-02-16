eba783a7fb52d5d48eefac9ac9fd7aaf8fa134f6
Devin Bombay <dbombay@bdtrust.org>
2019-02-16 16:53:46 -0500

    adds react, bootstrap, and dependencies

--- src/Controller/SecuredController.php
+++ src/Controller/SecuredController.php
@@ -13,36 +13,6 @@ class SecuredController extends AbstractController
      */
     public function publicAction()
     {
-
-        $data = [
-            [
-                'albumId' => "1",
-                "id" => 1,
-                "title" => "accusamus beatae ad facilis cum similique qui sunt",
-                "description" => "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout"
-            ],
-            [
-                'albumId' => "2",
-                "id" => 2,
-                "title" => "accusamus beatae ad facilis cum similique qui sunt",
-                "description" => "Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text"
-            ],
-            [
-                'albumId' => "3",
-                "id" => 3,
-                "title" => "accusamus beatae ad facilis cum similique qui sunt",
-                "description" => "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form"
-            ],
-        ];
-
-        return new JsonResponse($data);
-    }
-
-    /**
-     * @Route("/api/private", name="private")
-     */
-    public function privateAction()
-    {
         $data = [
             [
                 'albumId' => "1",
