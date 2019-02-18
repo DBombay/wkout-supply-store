1b0aa15911560badfae722feac89a429d09f349e
Devin Bombay <dbombay@bdtrust.org>
2019-02-17 22:16:12 -0500

    Makes API controller and product controller. Dummy json object for now

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class ApiController
+{
+
+    /**
+     * @var integer HTTP status code - 200 (OK) by default
+     */
+    protected $statusCode = 200;
+
+    /**
+     * Gets the value of statusCode.
+     *
+     * @return integer
+     */
+    public function getStatusCode()
+    {
+        return $this->statusCode;
+    }
+
+    /**
+     * Sets the value of statusCode.
+     *
+     * @param integer $statusCode the status code
+     *
+     * @return self
+     */
+    protected function setStatusCode($statusCode)
+    {
+        $this->statusCode = $statusCode;
+
+        return $this;
+    }
+
+    /**
+     * Returns a JSON response
+     *
+     * @param array $data
+     * @param array $headers
+     *
+     * @return Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function respond($data, $headers = [])
+    {
+        return new JsonResponse($data, $this->getStatusCode(), $headers);
+    }
+
+    /**
+     * Sets an error message and returns a JSON response
+     *
+     * @param string $errors
+     *
+     * @return Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function respondWithErrors($errors, $headers = [])
+    {
+        $data = [
+            'errors' => $errors,
+        ];
+
+        return new JsonResponse($data, $this->getStatusCode(), $headers);
+    }
+
+    /**
+     * Returns a 401 Unauthorized http response
+     *
+     * @param string $message
+     *
+     * @return Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function respondUnauthorized($message = 'Not authorized!')
+    {
+        return $this->setStatusCode(401)->respondWithErrors($message);
+    }
+}
---
+++ src/Controller/ProductController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends ApiController
+{
+    /**
+     * @Route("/products")
+     */
+    public function productsAction()
+    {
+        return $this->respond([
+            [
+                'Id' => 1,
+                'Name' => 'Dumbell 9000',
+                'Description' => 'Just when you thought the Dumbell 8999 didn\'t go far enough, the engineers at Universal Fit are proud to present the next evolution in picking things up and putting them down!',
+                'Price' => 99.00,
+                'Sale Price' => 0.1,
+                'Active' => true,
+                'On Sale' => true,
+                'Category' => 1,
+                'Sub-Category' => 1,
+                'Product Image' => 'http://dummyimage.com/100x100.jpg/cc0000/ffffff'
+            ]
+        ]);
+    }
+}
